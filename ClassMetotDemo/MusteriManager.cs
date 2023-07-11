8a701944885e94e58fca4bdf8c3111920972e335
Tugce BAYIRLI <tbayirli@hotmail.com>
2023-07-11 20:43:31 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        Musteri[] musteriler = new Musteri[] { };
+        List<Musteri> musteriList = new List<Musteri>();
+        public void MusteriEkle(Musteri musteri)
+        {
+            musteriList.Add(musteri);   
+            Console.WriteLine(musteri.Name + " Added");
+
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            musteriList.Remove(musteri);
+            Console.WriteLine(musteri.Id+" Deleted");
+        }
+
+        public void MusteriList()
+        {
+            foreach (Musteri musteri in musteriList)
+            {
+                Console.WriteLine(musteri.Id+" "+musteri.Name+" "+musteri.Surname+" "+musteri.Age);
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Musteri musteri1 = new Musteri();
+            musteri1.Id= 1; 
+            musteri1.Name = "ASD";
+            musteri1.Surname = "FGH";
+            musteri1.Age = 30;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Name = "ZXC";
+            musteri2.Surname = "VBN";
+            musteri2.Age = 42;
+
+
+            
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+
+            musteriManager.MusteriList();
+            //musteriManager.MusteriList(musteri2);
+
+            musteriManager.MusteriSilme(musteri1);
+            musteriManager.MusteriSilme(musteri2);
+        }
+    }
+}
